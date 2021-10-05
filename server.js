4cb6bcd4a974aa09145a56b6423abaacf203f35f
Satyam <satyamdhote20@gmail.com>
2021-10-05 15:48:47 +0530

    Implemented different routes and Search functionality

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/mybrary");
@@ -17,5 +20,7 @@ db.on("error", (error) => console.log(error));
 db.once("open", () => console.log("Connected to mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+app.use("/authors/new", authorRouter);
 
 app.listen(process.env.PORT || 3000);
